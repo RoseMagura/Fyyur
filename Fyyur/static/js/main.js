0b5f37280fede751e23e9a4a20a60b3e649f7115
RoseMagura <magurarm@guilford.edu>
2021-07-13 20:45:34 -0400

    Feat: Gather JS functions in one file

---
+++ Fyyur/static/js/main.js
@@ -0,0 +1,54 @@
+const deleteArtist = event => {
+  fetch("/artists/" + event.target.value, { method: "DELETE" })
+    .then(
+      setTimeout(() => {
+        window.location.href = "/artists";
+      }, 1000)
+    );
+}
+
+const deleteVenue = e => {
+  venueId = e.target.value;
+  fetch("/venues/" + venueId, { method: "DELETE" }).then(
+    setTimeout(function () {
+      window.location.href = "/venues";
+    }, 1000)
+  );
+}
+
+const deleteShow = evt => {
+  const showId = evt.target.value;
+  fetch("/shows/" + showId, { method: "DELETE" }).then(
+    setTimeout(() => {
+      location.reload();
+    }, 1000)
+  );
+}
+
+const openForm = () => {
+  location.pathname += '/edit';
+}
+
+const deleteArtistBtn = document.getElementById('delete-artist');
+const editBtns = document.getElementsByClassName('edit');
+
+const deleteShowBtns = document.getElementsByClassName('delete-show');
+console.log(deleteShowBtns);
+
+const deleteVenueBtn = document.getElementById('delete-venue');
+
+if (deleteArtistBtn !== null) {
+  deleteArtistBtn.addEventListener('click', deleteArtist);
+}
+
+for (let i = 0; i < editBtns.length; i++) {
+  editBtns[i].addEventListener('click', openForm);
+}
+
+if (deleteVenueBtn !== null) {
+  deleteVenueBtn.addEventListener('click', deleteVenue);
+}
+
+for (let counter = 0; counter < deleteShowBtns.length; counter++) {
+  deleteShowBtns[counter].addEventListener('click', deleteShow);
+}
